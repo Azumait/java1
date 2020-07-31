dccb5f609418704ff716b8c9a9a01add75215120
azumateam <ydhvocal@naver.com>
2020-07-31 12:28:25 +0900

    문자열타입의 처리

---
+++ src/java1/java6.java
@@ -0,0 +1,12 @@
+package java1;
+
+public class java6 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World".length()); // 11
+		System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
+
+	}
+
+}

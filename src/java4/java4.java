4de9fb0cd32aee97f6f413e584f4c3c2b336bf54
azumateam <ydhvocal@naver.com>
2020-08-10 15:51:18 +0900

    생성자(Contructor)와 this. 복습

---
+++ src/java4/java4.java
@@ -0,0 +1,14 @@
+package java4;
+
+public class java4 {
+	public static void main(String[] args) {
+		java4_print p1 = new java4_print("////");
+
+		java4_print p2 = new java4_print("****");
+
+		p1.A();
+		p1.B();
+		p2.A();
+		p2.B();
+	}
+}

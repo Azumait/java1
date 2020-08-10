a081bfc6ba851b84ff8986584622cda403337820
azumateam <ydhvocal@naver.com>
2020-08-10 17:37:22 +0900

    <패키지, 클래스명 정리>

---
+++ src/java3_advanced_grammer/java3_for_while.java
@@ -0,0 +1,24 @@
+package java3_advanced_grammer;
+
+public class java3_for_while {
+
+    public static void main(String[] args) {
+
+    	// 반복문 (for, while)
+        System.out.println("=== while ===");
+        int i = 0;
+        //..
+        while(i < 3) {
+            System.out.println(1);
+//          i = i + 1;
+            //..
+            i++;
+        }
+        System.out.println("=== for ===");
+        for(int j=0; j < 3; j++) {
+            System.out.println(2);
+        }
+
+    }
+
+}
